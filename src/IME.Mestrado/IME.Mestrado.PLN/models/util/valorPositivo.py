7f3d57d446f1f06947c13893bad5bbcb2cab8a20
Wallace da Silva Coelho Valadão <silvawallace2009@hotmail.com>
2022-11-16 07:26:54 -0300

    Correção para o array de respostas

--- src/IME.Mestrado/IME.Mestrado.PLN/models/util/valorPositivo.py
+++ src/IME.Mestrado/IME.Mestrado.PLN/models/util/valorPositivo.py
@@ -3,14 +3,16 @@ def converterArray(array):
     minUser = 0
 
     for item in array:
-        for atual in item:
-            if atual < minUser:
-                minUser = atual
+        for item2 in item:
+            for atual in item2:
+                if atual < minUser:
+                    minUser = atual
 
     minUser = minUser * -1
 
     for item in array:
-        for i in range(0, len(item)):
-            item[i] += minUser
+        for item2 in item:
+            for i in range(0, len(item2)):
+                item2[i] += minUser
 
     return array
