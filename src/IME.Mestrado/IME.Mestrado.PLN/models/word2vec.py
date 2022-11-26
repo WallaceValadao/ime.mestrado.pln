7eb955bc5075f49616df6cee40a33841109af19a
Wallace da Silva Coelho Valadão <silvawallace2009@hotmail.com>
2022-11-26 15:42:30 -0300

    Correção na geração do w2v

--- src/IME.Mestrado/IME.Mestrado.PLN/models/word2vec.py
+++ src/IME.Mestrado/IME.Mestrado.PLN/models/word2vec.py
@@ -2,9 +2,11 @@ import numpy as np
 from gensim.models import Word2Vec
 from os.path import exists
 import models.util.valorPositivo as valorPositivo
+import models.util.tratarCorteFrases as tratarFrases
 
 class PreProcessamentoW2v():
-    def __init__(self, configModel):
+    def __init__(self, config, configModel):
+        self.config = config
         self.configModel = configModel
 
         if not exists(configModel.pathModel):
@@ -16,19 +18,45 @@ class PreProcessamentoW2v():
         self.word_vectors.init_sims(replace=True)
 
 
-    def w2vEmbeddings(self, textos):
-        textos = [sentence.split(' ') for sentence in textos]
-        textos_sum = [[0]*self.word_vectors.vector_size for sentence in textos] # sentences não é usada, mas parece que funciona como equivalente a len(textos)
-        for i, sentence in enumerate(textos):
-            for word in sentence:
-                try:
-                    textos_sum[i] = textos_sum[i] + self.word_vectors[word]
-                except:
-                    continue
-            textos_sum[i] = np.array(textos_sum[i])/len(sentence)
+    def w2vEmbeddings(self, frases):
+        result = []
+
+        for textBase in frases:
+            for textos in tratarFrases.obterCorteFrases(self.config, textBase, self.configModel.size_vector):
+                textos = [sentence.split(' ') for sentence in textos]
+                textos_sum = [[0]*self.word_vectors.vector_size for sentence in textos] # sentences não é usada, mas parece que funciona como equivalente a len(textos)
+                for i, sentence in enumerate(textos):
+                    for word in sentence:
+                        try:
+                            textos_sum[i] = textos_sum[i] + self.word_vectors[word]
+                        except:
+                            continue
+                    textos_sum[i] = np.array(textos_sum[i])/len(sentence)
+            result.append(textos_sum)
     
-        textos_sum = valorPositivo.converterArray(textos_sum)
-        return [ np.array(textos_sum).tolist() ]
+        result = valorPositivo.converterArray(result)
+        return list(result)
+
+    def w2vEmbeddings2(self, textos):
+        textosPalavras = [sentence.split(' ') for sentence in textos]
+        textos_sum = [[0]*self.word_vectors.vector_size for sentence in textosPalavras] # sentences não é usada, mas parece que funciona como equivalente a len(textos)
+        
+        result = []
+        for i, sentence in enumerate(textos):
+            resultText = []
+
+            for text in tratarFrases.obterCorteFrases(self.config, sentence, self.configModel.size_vector):
+                for word in text.split(' '):
+                    try:
+                        resultText.append(textos_sum[i] + self.word_vectors[word]);
+                    except:
+                        continue
+                #textos_sum[i] = np.array(textos_sum[i])/len(sentence)
+
+            result.append(resultText)
+    
+        result = valorPositivo.converterArray(result)
+        return list(result)
     
     
     ## baseado no código de LuizGFerreira em https://github.com/Luizgferreira/subjectivity-classifier
